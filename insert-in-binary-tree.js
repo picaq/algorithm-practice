b769f2aa721e5a10a3c5e493830df1a60839be3d
Mandy <mandychen.art@gmail.comm>
2021-04-19 21:22:49 -0700

    write comments for solution inserting value into binary tree

---
+++ insert-in-binary-tree.js
@@ -0,0 +1,18 @@
+let insertBiTree = (val, tree) => {
+    // create pointer variable and copy tree into it
+
+    // create a while loop that checks for the truthiness of pointer in order to continue looping
+        // if val > pointer value AND the right of the pointer is not null
+            // move pointer to the right
+
+        // otherwise if the left of the pointer is not null
+            // move pointer to the left
+        
+        // otherwise
+            // if left of pointer is null
+                // set the left of pointer to val
+            // else if right of pointer is null
+                // set the right of pointer to val
+
+    // return tree
+}
